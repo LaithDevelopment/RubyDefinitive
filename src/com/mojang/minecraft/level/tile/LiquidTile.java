2d66817c98cfebcded30ec2202c0eb116143d758
LaithDevelopment <fharley66@gmail.com>
2020-12-04 19:51:13 +0000

    LiquidTile optimisations

--- src/com/mojang/minecraft/level/tile/LiquidTile.java
+++ src/com/mojang/minecraft/level/tile/LiquidTile.java
@@ -16,6 +16,7 @@ public class LiquidTile extends Tile {
     
     @Override
     public void tick(final Level level, final int x, final int y, final int z, final Random random) {
+    	//if adjacent block is airTile, turn into tileId
         if (level.getTile(x - 1, y, z) == 0 && level.getTile(x - 1, y + 1, z) != 0) {
             level.setTile(x-1, y, z, this.tileId);
         }
@@ -34,12 +35,15 @@ public class LiquidTile extends Tile {
         if (level.getTile(x, y + 1, z) == 0) {
             level.setTile(x, y, z, this.calmTileId);
         }
+        //if waterTile or calmWaterTile is below lavaTile, turn into lavaTile
         if(tileId == 3 && level.getTile(x, y+1, z) == 5 || tileId == 3 && level.getTile(x, y+1, z) == 6) {
         	level.setTile(x, y, z, 5);
         }
+        //if lavaTile or calmLavaTile is below WaterTile, turn into waterTile
         if(tileId == 5 && level.getTile(x, y+1, z) == 3 || tileId == 5 && level.getTile(x, y+1, z) == 4) {
         	level.setTile(x, y, z, 3);
         }
+        //if waterTile is next to LavaTile, turn adjacent lavaTile into rockTile
         if(this.tileId == 3) {
             if (level.getTile(x - 1, y, z) == 5) {
                 level.setTile(x-1, y, z, 2);
@@ -53,6 +57,38 @@ public class LiquidTile extends Tile {
             if (level.getTile(x, y, z + 1) == 5) {
                 level.setTile(x, y, z+1, 2);
             }
+        }
+    }
+    
+    @Override
+    public void render(final Tesselator t, final Level level, final int layer, final int x, final int y, final int z) {
+        final float c1 = 1.0f;
+        final float c2 = 0.8f;
+        final float c3 = 0.6f;
+        
+        if (level.getTile(x, y - 1, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y - 1, z) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y - 1, z) == 6 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c1, c1, c1);
+            this.renderFace(t, x, y, z, 0);
+        }
+        if (level.getTile(x, y + 1, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y + 1, z) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y + 1, z) == 6 && level.isLit(x, y, z) ^ layer == 1 ) {
+            t.color(c1, c1, c1);
+            this.renderFace(t, x, y, z, 1);
+        }
+        if (level.getTile(x, y, z - 1) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y, z - 1) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y, z - 1) == 6 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c2, c2, c2);
+            this.renderFace(t, x, y, z, 2);
+        }
+        if (level.getTile(x, y, z + 1) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y, z + 1) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y, z + 1) == 6 && level.isLit(x, y, z) ^ layer == 1 ) {
+            t.color(c2, c2, c2);
+            this.renderFace(t, x, y, z, 3);
+        }
+        if (level.getTile(x - 1, y, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x - 1, y, z) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x - 1, y, z) == 6 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c3, c3, c3);
+            this.renderFace(t, x, y, z, 4);
+        }
+        if (level.getTile(x + 1, y, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x + 1, y, z) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x + 1, y, z) == 6 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c3, c3, c3);
+            this.renderFace(t, x, y, z, 5);
         }
     }
     
