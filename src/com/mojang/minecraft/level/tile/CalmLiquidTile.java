cfaf181d18b10f30747171d7d25d45416faec8eb
LaithDevelopment <fharley66@gmail.com>
2020-12-04 19:51:21 +0000

    CalmLiquidTile optimisations

--- src/com/mojang/minecraft/level/tile/CalmLiquidTile.java
+++ src/com/mojang/minecraft/level/tile/CalmLiquidTile.java
@@ -14,6 +14,7 @@ public class CalmLiquidTile extends LiquidTile {
     
     @Override
     public void tick(final Level level, final int x, final int y, final int z, final Random random) {
+    	//if adjacent block is airTile, turn into calmTileId
         if (level.getTile(x - 1, y, z) == 0) {
             level.setTile(x-1, y, z, this.calmTileId);
         }
@@ -32,12 +33,15 @@ public class CalmLiquidTile extends LiquidTile {
         if (level.getTile(x, y + 1, z) != 0) {
             level.setTile(x, y, z, this.tileId);
         }
+        //if waterTile or calmWaterTile is below lavaTile, turn into lavaTile
         if(tileId == 3 && level.getTile(x, y+1, z) == 5 || tileId == 3 && level.getTile(x, y+1, z) == 6) {
         	level.setTile(x, y, z, 5);
         }
+        //if lavaTile or calmLavaTile is below WaterTile, turn into waterTile
         if(tileId == 5 && level.getTile(x, y+1, z) == 3 || tileId == 5 && level.getTile(x, y+1, z) == 4) {
         	level.setTile(x, y, z, 3);
         }
+        //if waterTile is next to LavaTile, turn adjacent lavaTile into rockTile
         if(this.calmTileId == 4) {
             if (level.getTile(x - 1, y, z) == 6) {
                 level.setTile(x-1, y, z, 2);
@@ -52,6 +56,7 @@ public class CalmLiquidTile extends LiquidTile {
                 level.setTile(x, y, z+1, 2);
             }
         }
+        //if waterTile is next to LavaTile, turn adjacent lavaTile into rockTile
         if(this.tileId == 3) {
             if (level.getTile(x - 1, y, z) == 5) {
                 level.setTile(x-1, y, z, 2);
@@ -93,6 +98,7 @@ public class CalmLiquidTile extends LiquidTile {
             t.vertexUV(x2, y3-0.1F, z2, u0, v0);
             t.vertexUV(x2, y3-0.1F, z3, u0, v2);
         }
+        //render sides 1 pixel lower than normal
         if (face == 2) {
             t.vertexUV(x2, y3-0.1F, z2, u2, v0);
             t.vertexUV(x3, y3-0.1F, z2, u0, v0);
@@ -118,6 +124,41 @@ public class CalmLiquidTile extends LiquidTile {
             t.vertexUV(x3, y3-0.1F, z3, u0, v0);
         }
     }
+    @Override
+    public void render(final Tesselator t, final Level level, final int layer, final int x, final int y, final int z) {
+        final float c1 = 1.0f;
+        final float c2 = 0.8f;
+        final float c3 = 0.6f;
+        
+        if (level.getTile(x, y - 1, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y - 1, z) == 4 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y - 1, z) == 6 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c1, c1, c1);
+            this.renderFace(t, x, y, z, 0);
+        }
+        if (level.getTile(x, y + 1, z) == 0 && level.isLit(x, y, z) ^ layer == 1 || level.getTile(x, y + 1, z) != 0 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c1, c1, c1);
+            this.renderFace(t, x, y, z, 1);
+        }
+        if (level.getTile(x, y, z - 1) == 0 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c2, c2, c2);
+            this.renderFace(t, x, y, z, 2);
+        }
+        if (level.getTile(x, y, z + 1) == 0 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c2, c2, c2);
+            this.renderFace(t, x, y, z, 3);
+        }
+        if (level.getTile(x - 1, y, z) == 0 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c3, c3, c3);
+            this.renderFace(t, x, y, z, 4);
+        }
+        if (level.getTile(x + 1, y, z) == 0 && level.isLit(x, y, z) ^ layer == 1) {
+            t.color(c3, c3, c3);
+            this.renderFace(t, x, y, z, 5);
+        }
+    }
+    
+    private boolean shouldRenderFace(final Level level, final int x, final int y, final int z, final int layer) {
+        return !level.isSolidTile(x, y, z) && (level.isLit(x, y, z) ^ layer == 1);
+    }
     
     @Override
     public AABB getAABB(final int x, final int y, final int z) {
@@ -128,9 +169,8 @@ public class CalmLiquidTile extends LiquidTile {
     public boolean blocksLight() {
         return false;
     }
-    
     @Override
     public boolean isSolid() {
-        return false;
+    	return false;
     }
 }
